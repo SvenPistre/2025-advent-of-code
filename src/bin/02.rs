f756e26d4ed023555016384b305e1f5446ecb858
Sven Pistre <100095453+SvenPistre@users.noreply.github.com>
2025-12-02 22:55:04 +0000

    feat: solve day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -52,8 +52,57 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(invalid_ids.iter().sum::<u64>())
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+mod part_two {
+    use super::*;
+
+    pub fn find_invalid_ids(range: &Range) -> Vec<u64> {
+        let mut invalid_ids = vec![];
+        for id in range.0..=range.1 {
+            if is_invalid_id(id) {
+                invalid_ids.push(id);
+            }
+        }
+        invalid_ids
+    }
+
+    fn is_invalid_id(num: u64) -> bool {
+        if num < 10 {
+            return false;
+        }
+        let num_str = num.to_string();
+        let mut divisors = divisors(num_str.len());
+        divisors.remove(0);
+        divisors.iter().any(|&divisor| {
+            let (first, _) = num_str.split_at(num_str.len() / divisor);
+            first.repeat(divisor).parse::<u64>().unwrap() == num
+        })
+    }
+
+    pub fn divisors(num: usize) -> Vec<usize> {
+        let mut divs = vec![];
+        let mut i = 1;
+        while i * i <= num {
+            if num.is_multiple_of(i) {
+                divs.push(i);
+                if i != num / i {
+                    divs.push(num / i);
+                }
+            }
+            i += 1;
+        }
+        divs.sort();
+        divs
+    }
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let ranges: Vec<Range> = input
+        .split(',')
+        .map(Range::parse)
+        .map(|res| res.expect("input should be valid"))
+        .collect();
+    let invalid_ids: Vec<u64> = ranges.iter().flat_map(part_two::find_invalid_ids).collect();
+    Some(invalid_ids.iter().sum::<u64>())
 }
 
 #[cfg(test)]
@@ -69,7 +118,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 
     #[test]
@@ -86,6 +135,27 @@ mod tests {
         ];
         ranges.iter().for_each(|(range, expected)| {
             let actual = part_one::find_invalid_ids(range);
+            assert_eq!(actual, *expected);
+        });
+    }
+
+    #[test]
+    fn test_part_two_find_invalid_ids_works() {
+        let ranges: Vec<(Range, Vec<u64>)> = vec![
+            (Range(11, 22), vec![11, 22]),
+            (Range(95, 115), vec![99, 111]),
+            (Range(998, 1012), vec![999, 1010]),
+            (Range(1188511880, 1188511890), vec![1188511885]),
+            (Range(222220, 222224), vec![222222]),
+            (Range(1698522, 1698528), vec![]),
+            (Range(446443, 446449), vec![446446]),
+            (Range(38593856, 38593862), vec![38593859]),
+            (Range(565653, 565659), vec![565656]),
+            (Range(824824821, 824824827), vec![824824824]),
+            (Range(2121212118, 2121212124), vec![2121212121]),
+        ];
+        ranges.iter().for_each(|(range, expected)| {
+            let actual = part_two::find_invalid_ids(range);
             assert_eq!(actual, *expected);
         });
     }
